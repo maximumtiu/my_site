f7b402f6a9cfb72ff7d0e845a83334774a96b973
Megan Tiu <megantiu@me.com>
2015-08-18 13:28:49 -0400

    Added create_contact to helper

--- app/helpers/welcome_helper.rb
+++ app/helpers/welcome_helper.rb
@@ -1,2 +1,16 @@
 module WelcomeHelper
+	def create_contact
+  	contact = Contact.new
+  	contact.name = params[:name]
+  	contact.email = params[:email]
+  	contact.message = params[:message]
+  	contact.deliver
+
+  	#if contact
+  		#params[:success] = true
+  		@success = 'Thank you! Message sent.'
+  		redirect_to contact_path, :notice => @success
+  		# contact_path, notice: 'yay'
+  	#end
+  end
 end
