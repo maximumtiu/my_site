cc7ee1fcc193d6f24858bf1524588ea1f5883283
Megan Tiu <megantiu@me.com>
2016-03-04 09:00:35 -0500

    Cleaning up some things, renaming welcome to static

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,12 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+  root 'static#index'
 
-  get 'about' => 'welcome#about'
+  get 'about' => 'static#about'
+  get 'contact' => 'static#contact'
+  get 'test' => 'static#test'
 
-  get 'contact' => 'welcome#contact'
-
-  get 'test' => 'welcome#test'
-
-  post 'contact' => 'welcome#contact'
-
-  #get 'contact2' => 'contact#contact2'
-  #post 'contact2' => 'contact#contact2'
-  #post 'contacts' => 'contact#contact2'
-  post 'create_contact' => 'welcome#create_contact'
-
+  resources :blog
   get 'blog' => 'blog#index'
   get 'blog/1' => 'blog#post_one'
   get 'blog/2' => 'blog#post_two'
