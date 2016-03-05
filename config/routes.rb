4cd5aa8cca7af878f238df67d246a3924d52b8c0
Megan Tiu <megantiu@me.com>
2016-03-04 20:23:04 -0500

    Further cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   get 'contact' => 'static#contact'
   get 'test' => 'static#test'
 
-  resources :blog
   get 'blog' => 'blog#index'
   get 'blog/1' => 'blog#post_one'
   get 'blog/2' => 'blog#post_two'
