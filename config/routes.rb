896b336fc1fabef15e30077fa2190387db27e0e3
Megan Tiu <megantiu@me.com>
2016-05-29 17:34:51 -0400

    Publishing post 5

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,13 @@ Rails.application.routes.draw do
   get 'blog/2' => 'blog#post_two'
   get 'blog/3' => 'blog#post_three'
   get 'blog/4' => 'blog#post_four'
-  # get 'blog/5' => 'blog#post_five'
+  get 'blog/5' => 'blog#post_five'
+  get 'blog/6' => 'blog#post_six'
 
   get 'blog/1/the-five-most-meaningful-lessons-from-coding-bootcamp' => 'blog#post_one'
   get 'blog/2/on-the-importance-of-side-projects'                    => 'blog#post_two'
   get 'blog/3/blog-buddies'                                          => 'blog#post_three'
   get 'blog/4/getting-and-setting-cookies-in-javascript'             => 'blog#post_four'
-  # get 'blog/5/adventures-of-a-one-woman-dev-team'                    => 'blog#post_five'
+  get 'blog/5/rubys-enumerable-module-and-you'                       => 'blog#post_five'
+  get 'blog/6/adventures-of-a-one-woman-dev-team'                    => 'blog#post_six'
 end
