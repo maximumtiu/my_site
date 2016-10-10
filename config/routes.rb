93313f37558a4ed15126f1c1ed314c46ca671e86
Megan Tiu <megantiu@me.com>
2016-10-10 10:03:12 -0400

    Unpublishing the first two posts

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   get 'talks'  => 'static#talks'
 
   get 'blog'    => 'blog#index'
-  get 'blog/1'  => 'blog#post_one'
-  get 'blog/2'  => 'blog#post_two'
   get 'blog/3'  => 'blog#post_three'
   get 'blog/4'  => 'blog#post_four'
   get 'blog/5'  => 'blog#post_five'
@@ -19,8 +17,6 @@ Rails.application.routes.draw do
   get 'blog/10' => 'blog#post_ten'
   get 'blog/11' => 'blog#post_eleven'
 
-  get 'blog/1/the-five-most-meaningful-lessons-from-coding-bootcamp' => 'blog#post_one'
-  get 'blog/2/on-the-importance-of-side-projects'                    => 'blog#post_two'
   get 'blog/3/blog-buddies'                                          => 'blog#post_three'
   get 'blog/4/getting-and-setting-cookies-in-javascript'             => 'blog#post_four'
   get 'blog/5/rubys-enumerable-module-and-you'                       => 'blog#post_five'
